b174017b14acdd1a6cec92144179d771136282b5
victbsb <v.tabaressibille@gmail.com>
2023-02-02 18:46:21 +0100

    actualizacion

--- JavaScript/ejercicio2.js
+++ JavaScript/ejercicio2.js
@@ -1,6 +1,6 @@
 const celta = {
-    dia: 15,
-    mes: 'noviembre', 
+    dia: 30,
+    mes: 'febrero', 
 };
 
-console.log(`mi cumpleaños es el ${celta.dia} ${celta.mes}`)
+console.log(`mi cumpleaños es el ${celta.dia} de ${celta.mes}`)
---
+++ JavaScript/ejercicio4.js
@@ -0,0 +1,7 @@
+for(let i= 50; i >=20; i--){
+    if (i%2 == 0){
+        console.log(i)
+    };
+};
+
+
