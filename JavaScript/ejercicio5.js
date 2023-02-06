34d4015b646dd61bb66373827ad00d5906799e12
victbsb <v.tabaressibille@gmail.com>
2023-02-06 16:33:33 +0100

    formulario como ejercicio en clase

--- JavaScript/ejercicio5.js
+++ JavaScript/ejercicio5.js
@@ -1,6 +1,19 @@
-const meses = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto,', 'septiembre', 'octubre', 'noviembre', 'diciembre'];
+const meses = [
+  "enero",
+  "febrero",
+  "marzo",
+  "abril",
+  "mayo",
+  "junio",
+  "julio",
+  "agosto,",
+  "septiembre",
+  "octubre",
+  "noviembre",
+  "diciembre",
+];
 const longMeses = meses
-    .filter(mes => mes.length > 7)
-    .map(mes => mes.toUpperCase());
+  .filter((mes) => mes.length > 7)
+  .map((mes) => mes.toUpperCase());
 
-console.log(longMeses)
+console.log(longMeses);
---
+++ JavaScript/ejercicio6.js
@@ -0,0 +1,14 @@
+const environmental = "environmental";
+environmental[0].toUpperCase() + environmental.substring(1);
+
+const systems = "systems";
+systems[0].toUpperCase() + systems.substring(1);
+
+const research = "research";
+research[0].toUpperCase() + research.substring(1);
+
+const institute = "institute";
+institute[0].toUpperCase() + institute.substring(1);
+
+console.log(`${environmental} ${systems} ${research} ${institute}`)
+
